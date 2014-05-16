6222c6475f194b3198a202c0f91725dd983288bb
NgaNguyenDuy <kyo1508@gmail.com>
2014-05-16 17:00:50 +0700

    delete something

--- myPrj/testphp/probux/js/processform.js
+++
@@ -1,5 +0,0 @@
-function processForm(form){
-	form.setAttribute("onsubmit","return false;");
-	var inputs = form.getElementsByTagName("input"); 
-	for (var i = 0; i < inputs.length; i++) {if (inputs[i].type === 'submit') {inputs[i].disabled = true;inputs[i].value = "Processing...";inputs[i].className = "button-submit-d";}}
-}
--- myPrj/testphp/probux/ubar.php
+++
@@ -1,8 +0,0 @@
-<div style="margin:0 auto;width:980px;">
-     <table cellpadding="0" cellspacing="9" class="tbl-ubar">
-     <tr><td><a href="account.php" alt="My Account" title="My Account"><img src="images/ubar-acc.png"></a></td></tr>
-     <tr><td><a href="getref.php" alt="Your Referral Link" title="Your Ref Link"><img src="images/ubar-link.png"></a></td></tr>
-     <tr><td><a href="#"><img src="images/ubar-upg.png" alt="Upgrading your account!" title="Upgrading your account!"></a></td></tr>
-     <tr><td><a href="settings.php"><img src="images/ubar-set.png" alt="Personal Settings" title="Personal Settings"></a></td></tr>
-     </table>
-</div>
