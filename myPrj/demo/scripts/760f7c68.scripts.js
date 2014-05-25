d02d426be79274ca2d309c318e4ec15beafaa7ea
NgaNguyenDuy <kyo1508@gmail.com>
2014-05-25 16:52:13 +0700

    delete something

--- myPrj/demo/scripts/760f7c68.scripts.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("probuxApp",["ngCookies","ngResource","ngSanitize","ngRoute"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("probuxApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
